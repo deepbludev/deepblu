5c3c432b85d33e484d8d3c3bfea1614ef7f12aad
Carlo Casorzo <carlo@deepblu.dev>
2022-09-20 10:05:35 +0200

    refactor: change function to arrow fn

--- apps/demo/tasks/pages/api/hello.tsx
+++ apps/demo/tasks/pages/api/hello.tsx
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export const handle = async (req: NextApiRequest, res: NextApiResponse) => {
+const handle = async (req: NextApiRequest, res: NextApiResponse) => {
   res.json({ data: 'Hello World' })
 }
 
