0c966234dfb2a2e3f6a4174c2d3281b5a3c7fbb8
Carlo Casorzo <carlo@deepblu.dev>
2022-09-20 19:33:25 +0200

    test(vo): ensure ValueObject.create  creates a valid VO

--- libs/packages/ddd/src/core/spec/value-object.spec.ts
+++ libs/packages/ddd/src/core/spec/value-object.spec.ts
@@ -5,6 +5,10 @@ class TestValueObject extends ValueObject<PropType> {
   constructor(props: PropType) {
     super(props)
   }
+
+  public static override isValidProps(props: PropType): boolean {
+    return this.validator.string(props.foo)
+  }
 }
 
 class TestValueObject2 extends ValueObject<PropType> {
@@ -52,5 +56,20 @@ describe('ValueObject', () => {
   it('should be able to clone value objects', () => {
     const vo1 = vo.clone()
     expect(vo.equals(vo1)).toBeTruthy()
+  })
+
+  it('should be able to create a new instance', () => {
+    const result = TestValueObject.create({ foo: 'bar' })
+    expect(result.isOk).toBeTruthy()
+    expect(result.value).toEqual(new TestValueObject({ foo: 'bar' }))
+  })
+
+  it('should not be able to create a new instance with invalid props', () => {
+    const result = TestValueObject.create({ foo: 1 })
+    expect(result.error).toEqual(
+      new Error(
+        'Invalid props to create an instance of ' + TestValueObject.name
+      )
+    )
   })
 })
