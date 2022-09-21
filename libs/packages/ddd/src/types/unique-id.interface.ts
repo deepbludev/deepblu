acee79fc0950a82d6ee726ee924ec39f417dfb5d
Carlo Casorzo <carlo@deepblu.dev>
2022-09-21 18:02:03 +0200

    feat(uid): simplify uid interface

--- libs/packages/ddd/src/types/unique-id.interface.ts
+++ libs/packages/ddd/src/types/unique-id.interface.ts
@@ -1,9 +1,7 @@
 export interface IUniqueID {
   value: string
   isNew: boolean
-  createdAt: Date
   equal<T extends IUniqueID = IUniqueID>(id: T): boolean
-  deepEqual<T extends IUniqueID = IUniqueID>(id: T): boolean
-  cloneAsNew<T extends IUniqueID = IUniqueID>(): T
   clone<T extends IUniqueID = IUniqueID>(): T
+  cloneAsNew<T extends IUniqueID = IUniqueID>(): T
 }
