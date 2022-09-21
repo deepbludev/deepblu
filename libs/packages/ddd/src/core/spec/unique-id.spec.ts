207b4a3930f97b51f2b27928f084dc468dd988d3
Carlo Casorzo <carlo@deepblu.dev>
2022-09-21 18:05:31 +0200

    feat(uid): add UniqueID abstract class

---
+++ libs/packages/ddd/src/core/spec/unique-id.spec.ts
@@ -0,0 +1,35 @@
+import { UniqueID, UniqueIDProps } from '../unique-id.abstract'
+
+class MockUniqueID extends UniqueID {
+  constructor(props: UniqueIDProps) {
+    super(props)
+  }
+}
+
+describe('UniqueID', () => {
+  const id = new MockUniqueID({ value: '123', isNew: true })
+  const id2 = new MockUniqueID({ value: '123', isNew: false })
+  const id3 = new MockUniqueID({ value: '456', isNew: true })
+
+  it('should be defined', () => {
+    expect(UniqueID).toBeDefined()
+    expect(MockUniqueID).toBeDefined()
+  })
+  it('should be able to create a new instance', () => {
+    expect(id).toBeDefined()
+    expect(id.value).toEqual('123')
+    expect(id.isNew).toEqual(true)
+  })
+
+  it('should be able to compare UUIDs', () => {
+    expect(id.equal(id2)).toEqual(true)
+    expect(id.equal(id3)).toEqual(false)
+  })
+
+  it('should be able to clone UUIDs', () => {
+    const clone: MockUniqueID = id.clone()
+    expect(clone).toBeDefined()
+    expect(clone.value).toEqual('123')
+    expect(clone.isNew).toEqual(true)
+  })
+})
---
+++ libs/packages/ddd/src/core/unique-id.abstract.ts
@@ -0,0 +1,43 @@
+import { IUniqueID } from '../types/unique-id.interface'
+import { ValueObject, VOProps } from './value-object'
+
+export interface UniqueIDProps extends VOProps {
+  value: string
+  isNew: boolean
+}
+
+export abstract class UniqueID
+  extends ValueObject<UniqueIDProps>
+  implements IUniqueID
+{
+  protected constructor(props: UniqueIDProps) {
+    super(props)
+  }
+
+  equal(id: IUniqueID): boolean {
+    return typeof this.value === typeof id.value && this.value === id.value
+  }
+
+  override clone<UniqueID>(): UniqueID {
+    return super.clone() as UniqueID
+  }
+
+  cloneAsNew<UniqueID>(): UniqueID {
+    const constructor = Reflect.getPrototypeOf(this)?.constructor || UniqueID
+    const clone = Reflect.construct(constructor, [
+      {
+        value: this.value,
+        isNew: true,
+      },
+    ])
+    return clone
+  }
+
+  get value(): string {
+    return this.props.value
+  }
+
+  get isNew(): boolean {
+    return this.props.isNew
+  }
+}
