7dc8c530e8a32251964cb74489eded12a0961dea
Carlo Casorzo <carlo@deepblu.dev>
2022-09-22 15:56:32 +0200

    chore: add package exports

--- libs/packages/ddd/src/index.ts
+++ libs/packages/ddd/src/index.ts
@@ -1,1 +1,12 @@
+// core
 export * from './core/result'
+export * from './core/errors'
+export * from './core/domain-object.abstract'
+export * from './core/value-object'
+export * from './core/unique-id.abstract'
+export * from './core/uuid.vo'
+
+// types
+export * from './types/serializable.interface'
+export * from './types/domain-object.type'
+export * from './types/unique-id.interface'
