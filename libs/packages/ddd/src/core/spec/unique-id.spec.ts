7ce3ab365d41098a4844f5b22b5197375bf32cc4
Carlo Casorzo <carlo@deepblu.dev>
2022-09-22 18:47:20 +0200

    test(uid): use text strings instead of numbers in tests

--- libs/packages/ddd/src/core/spec/unique-id.spec.ts
+++ libs/packages/ddd/src/core/spec/unique-id.spec.ts
@@ -7,16 +7,16 @@ class MockUniqueID extends UniqueID {
 }
 
 describe('UniqueID', () => {
-  const id = new MockUniqueID({ value: '123', isNew: true })
-  const id2 = new MockUniqueID({ value: '123', isNew: false })
-  const id3 = new MockUniqueID({ value: '456', isNew: true })
+  const id = new MockUniqueID({ value: 'asd', isNew: true })
+  const id2 = new MockUniqueID({ value: 'asd', isNew: false })
+  const id3 = new MockUniqueID({ value: 'qwe', isNew: true })
 
   it('should be defined', () => {
     expect(UniqueID).toBeDefined()
     expect(MockUniqueID).toBeDefined()
   })
   it('should be able to create a new instance', () => {
-    expect(id.value).toEqual('123')
+    expect(id.value).toEqual('asd')
   })
 
   it('should be able to compare UUIDs', () => {
@@ -26,9 +26,9 @@ describe('UniqueID', () => {
 
   it('should be able to clone UUIDs', () => {
     const clone = id.clone<MockUniqueID>()
-    expect(clone.value).toEqual('123')
+    expect(clone.value).toEqual('asd')
 
     const clone2: MockUniqueID = id.clone()
-    expect(clone2.value).toEqual('123')
+    expect(clone2.value).toEqual('asd')
   })
 })
