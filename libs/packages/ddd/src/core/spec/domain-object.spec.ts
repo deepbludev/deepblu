d59760bc8ff383317ca9df4f137fe4018f9a1139
Carlo Casorzo <carlo@deepblu.dev>
2022-09-22 15:48:18 +0200

    style(do): rename dummy props in test

--- libs/packages/ddd/src/core/spec/domain-object.spec.ts
+++ libs/packages/ddd/src/core/spec/domain-object.spec.ts
@@ -1,19 +1,19 @@
 import { DomainObject } from '../domain-object.abstract'
 
 describe('DomainObject', () => {
-  type PropType = { foo: string; bar: string }
+  type DummyProps = { foo: string; bar: string }
 
-  class TestDO extends DomainObject<PropType> {
-    constructor(props: PropType) {
+  class TestDO extends DomainObject<DummyProps> {
+    constructor(props: DummyProps) {
       super(props, 'Entity')
     }
 
-    equals(p: DomainObject<PropType>): boolean {
+    equals(p: DomainObject<DummyProps>): boolean {
       return !!p
     }
   }
 
-  const props: PropType = { foo: 'bar', bar: 'baz' }
+  const props: DummyProps = { foo: 'bar', bar: 'baz' }
   const p = new TestDO(props)
   const p1 = new TestDO({ foo: 'bar', bar: 'baz' })
   const p2 = new TestDO({ foo: 'foo', bar: 'baz' })
@@ -45,13 +45,13 @@ describe('DomainObject', () => {
   it('should be able to compare props type', () => {
     expect(p.isSameClass(p1)).toBeTruthy()
     expect(p.isSameClass(p2)).toBeTruthy()
-    expect(p.isSameClass(p3 as DomainObject<PropType>)).toBeFalsy()
+    expect(p.isSameClass(p3 as DomainObject<DummyProps>)).toBeFalsy()
   })
 
   it('should be able to compare props', () => {
     expect(p.hasEqualProps(p1)).toBeTruthy()
     expect(p.hasEqualProps(p2)).toBeFalsy()
-    expect(p.isSameClass(p3 as DomainObject<PropType>)).toBeFalsy()
+    expect(p.isSameClass(p3 as DomainObject<DummyProps>)).toBeFalsy()
     expect(p.hasEqualProps(p4)).toBeTruthy()
   })
 
@@ -61,7 +61,7 @@ describe('DomainObject', () => {
 
   it('should be able to validate props', () => {
     expect(TestDO.isValidProps({ foo: 'bar', bar: 'baz' })).toBeTruthy()
-    expect(TestDO.isValidProps(null as unknown as PropType)).toBeFalsy()
-    expect(TestDO.isValidProps(undefined as unknown as PropType)).toBeFalsy()
+    expect(TestDO.isValidProps(null as unknown as DummyProps)).toBeFalsy()
+    expect(TestDO.isValidProps(undefined as unknown as DummyProps)).toBeFalsy()
   })
 })
