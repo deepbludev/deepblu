3aa03b324210897e0aacccc4e2e6bf3c6a31e886
Carlo Casorzo <carlo@deepblu.dev>
2022-09-25 09:48:29 +0200

    docs: add @id decorator docs

--- libs/ddd/core/src/core/unique-id.ts
+++ libs/ddd/core/src/core/unique-id.ts
@@ -53,13 +53,32 @@ export class UniqueID extends ValueObject<UniqueIDProps> implements IUniqueID {
   }
 }
 
+/**
+ * Decorator to create a unique ID class with a static create method
+ * with the given generator and validator functions.
+ * @example
+ * <pre>
+ * @id({
+ *  generator: () => new ObjectID().toHexString(),
+ *  validator: (id: string) => ObjectID.isValid(id)
+ * })
+ * class ObjectID extends UniqueID {
+ *   constructor(props: UniqueIDProps) {
+ *     super(props)
+ *   }
+ * }
+ *</pre>
+ *
+ * @param opts Options for the ID class. Object containing the generator and validator functions.
+ * @returns class decorator
+ */
 export const id = (opts: {
   generator: () => string
   validator: (id: string) => boolean
 }) =>
   function <
     // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-    T extends { new (...args: any[]): {} } & {
+    T extends typeof UniqueID & {
       generate: () => string
       validate: (id: string) => boolean
     }
