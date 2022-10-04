ff1e5a6ec02b4148cbcd478891363f881bb84556
Carlo Casorzo <carlo@deepblu.dev>
2022-10-04 15:35:28 +0200

    fix(ioc): remove deprecated files

--- libs/packages/ioc/src/deprecated/constants.ts
+++
@@ -1,3 +0,0 @@
-export enum ioc {
-  PROVIDER = 'ioc:provider',
-}
--- libs/packages/ioc/src/deprecated/provider.decorator.ts
+++
@@ -1,8 +0,0 @@
-import 'reflect-metadata'
-import { ioc } from './constants'
-
-export function provider(): ClassDecorator {
-  return (target: object) => {
-    Reflect.defineMetadata(ioc.PROVIDER, true, target)
-  }
-}
