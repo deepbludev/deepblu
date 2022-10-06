7231acfd321bdfeaf75d5af448b9dc34e723658a
Carlo Casorzo <carlo@deepblu.dev>
2022-10-06 07:52:12 +0200

    chore: update exports

--- libs/packages/ddd/src/domain-objects/index.ts
+++ libs/packages/ddd/src/domain-objects/index.ts
@@ -1,14 +1,23 @@
 // domain-objects
-export * from './base/result'
-export * from './base/errors'
+export * from './aggregate/base-aggregate.abstract'
+
 export * from './base/domain-object.abstract'
-export * from './value-object/value-object'
+export * from './base/domain-object.type'
+export * from './base/errors'
+export * from './base/result'
+export * from './base/result.interface'
+
+export * from './entity/entity'
+export * from './entity/base-entity.abstract'
+export * from './entity/unique.decorator'
+
+export * from './event/domain-event'
+export * from './event/event-from.decorator'
+export * from './event/event.interface'
+
+export * from './uid/custom-uid.decorator'
 export * from './uid/unique-id.vo'
 export * from './uid/unique-id.interface'
 export * from './uid/uuid.vo'
-export * from './uid/custom-uid.decorator'
-export * from './entity/unique.decorator'
-export * from './entity/base-entity.abstract'
-export * from './entity/entity'
-export * from './base/result.interface'
-export * from './base/domain-object.type'
+
+export * from './value-object/value-object'
