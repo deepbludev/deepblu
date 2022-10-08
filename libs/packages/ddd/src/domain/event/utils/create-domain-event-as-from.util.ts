a097a101c9ac79ae5cc62db512cd07f22007616c
Carlo Casorzo <carlo@deepblu.dev>
2022-10-08 09:57:31 +0200

    refactor: rename EventClass as DomainEventClass

--- libs/packages/ddd/src/domain/event/utils/create-domain-event-as-from.util.ts
+++ libs/packages/ddd/src/domain/event/utils/create-domain-event-as-from.util.ts
@@ -3,14 +3,14 @@ import { DomainEvent } from '../domain-event'
 export const createDomainEvent = <P = Record<string, never>>() => ({
   as: (event: string) => ({
     from: (aggregate: string) => {
-      const EventClass = class extends DomainEvent<P> {
+      const DomainEventClass = class extends DomainEvent<P> {
         static override aggregate = aggregate
         constructor(id: string, payload: P) {
           super(id, payload)
         }
       }
-      Reflect.defineProperty(EventClass, 'name', { value: event })
-      return EventClass
+      Reflect.defineProperty(DomainEventClass, 'name', { value: event })
+      return DomainEventClass
     },
   }),
 })
