98e93f089c5b87598f5b87986a0516d7a5cba744
Carlo Casorzo <carlo@deepblu.dev>
2022-10-18 15:09:30 +0200

    feat(utils): implement CustomNumber value object and @customNumber decorator

--- libs/packages/ddd/src/utils/value-objects/index.ts
+++ libs/packages/ddd/src/utils/value-objects/index.ts
@@ -1,4 +1,9 @@
-export * from './custom-string/custom-string.vo'
-export * from './custom-string/invalid-string.error'
+export * from './custom-number/custom-number.decorator'
+export * from './custom-number/invalid-number.error'
+export * from './custom-number/custom-number.vo'
+
 export * from './custom-string/custom-string.decorator'
+export * from './custom-string/invalid-string.error'
+export * from './custom-string/custom-string.vo'
+
 export * from './custom-text/custom-text.vo'
