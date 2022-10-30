8a72c740bee76a98f03dc35ffb832971e0407698
Carlo Casorzo <carlo@deepblu.dev>
2022-10-30 09:51:11 +0100

    test: add create tx handler failing test

--- libs/examples/transactions-app/contexts/core/transaction/application/src/commands/create/create.transaction.handler.spec.ts
+++ libs/examples/transactions-app/contexts/core/transaction/application/src/commands/create/create.transaction.handler.spec.ts
@@ -1,19 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing'
-
+import { createTxDTOStub } from '../../__mocks__/create.transaction.dto.stub'
+import { CreateTransaction } from './create.transaction.command'
 import { CreateTransactionHandler } from './create.transaction.handler'
 
 describe(CreateTransactionHandler, () => {
   let handler: CreateTransactionHandler
+  let repo: TransactionRepo
+  let saveSpy: jest.SpyInstance
+
+  const validTx = createTxDTOStub()
+  const invalidTx = createTxDTOStub({
+    amount: -1000.0,
+    id: 'invalid-uuid',
+    clientId: 'invalid-uuid',
+    currency: 'invalid-currency',
+    date: 'invalid-date',
+  })
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CreateTransactionHandler],
+      providers: [CreateTransactionHandler, TransactionRepoMock],
     }).compile()
 
     handler = module.get(CreateTransactionHandler)
+    repo = module.get(TransactionRepo)
+    saveSpy = jest.spyOn(repo, 'save')
   })
 
   it('should be defined', () => {
     expect(handler).toBeDefined()
+  })
+
+  describe('#handle', () => {
+    describe('when the command is valid', () => {
+      it('should delegate creation to TransactionRepo and return a successful result', async () => {
+        const result = await handler.handle(CreateTransaction.with(validTx))
+        expect(result.isOk).toBe(true)
+        expect(saveSpy).toHaveBeenCalledWith(validTx)
+      })
+    })
+
+    describe('when the command is valid', () => {
+      it('should fail to create a transaction', async () => {
+        const result = await handler.handle(CreateTransaction.with(invalidTx))
+        expect(result.isFail).toBe(true)
+        expect(saveSpy).not.toHaveBeenCalled()
+      })
+    })
   })
 })
