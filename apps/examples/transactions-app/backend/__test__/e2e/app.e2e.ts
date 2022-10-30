0fdf7281b0dd6c2df50893dc1393741b5ba8bbe4
Carlo Casorzo <carlo@deepblu.dev>
2022-10-30 08:33:53 +0100

    test(e2e): add failing test with full created tx response

--- apps/examples/transactions-app/backend/__test__/e2e/app.e2e.ts
+++ apps/examples/transactions-app/backend/__test__/e2e/app.e2e.ts
@@ -24,20 +24,44 @@ describe('AppController (e2e)', () => {
   describe('/transaction', () => {
     describe('POST', () => {
       describe('when touching endpoint', () => {
-        it('should return 200 when body is valid', () => {
+        it('should return 201 when body is valid', () => {
           return e2e
             .request()
             .post('/transaction')
             .send(createTxDTOStub())
             .expect(HttpStatus.CREATED)
+            .expect({
+              statusCode: HttpStatus.CREATED,
+              status: 'Transaction created successfully',
+              data: {
+                ...createTxDTOStub(),
+                commission: 0.5,
+                currency: 'EUR',
+              },
+            })
         })
 
-        it('should return 400 when body is invalid', () => {
+        it('should return 422 when body is invalid', () => {
           return e2e
             .request()
             .post('/transaction')
-            .send({ clientId: '1' })
+            .send({
+              date: '2022-10-241',
+              amount: -1000.0,
+              currency: 'USDA',
+              clientId: '88cc384c-eb13-4eee-af43-9f64c36f9e98_',
+            })
             .expect(HttpStatus.UNPROCESSABLE_ENTITY)
+            .expect({
+              statusCode: 422,
+              message:
+                'Input validation failed - ' +
+                'date: Date must be a valid date, ' +
+                'amount: Amount must be a positive number, ' +
+                'currency: Currency must be a valid currency (e.g. USD, EUR, GBP, JPY), ' +
+                'clientId: Client ID must be a valid UUID',
+              error: 'Unprocessable Entity',
+            })
         })
       })
     })
