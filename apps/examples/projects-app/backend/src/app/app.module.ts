7025b5f5a8a9d98537ce746f49c44ee9352afc9b
Carlo Casorzo <56896917+deepblue369@users.noreply.github.com>
2022-11-03 15:41:30 +0100

    chore: remove projects-app

--- apps/examples/projects-app/backend/src/app/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common'
-
-import { StatusController } from './status.controller'
-
-@Module({
-  imports: [],
-  controllers: [StatusController],
-})
-export class AppModule {}
